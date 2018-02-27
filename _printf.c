2221c5fe77d0ed10c331e548ef4bea7928eeac3d
maxastuart <maxastuart@gmail.com>
2018-02-27 02:18:16 +0000

    1.0 MVP, still lots of bugs

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+
+/**
+ * printf - takes a string and args of each '%s' and '%c and prints them
+ * @format: initial string containing % + char denoting type and number of args
+ * @...: variable list of arguments
+ *
+ * Return: number of characters printed.
+ */
+int _printf(const char * format, ...)
+{
+	int i, j, count = 0;
+	char cbuf;
+/*	int *ibuf = 0; */
+	char *sbuf;
+	va_list lst;
+
+	va_start(lst, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				count += 1;
+				cbuf = va_arg(lst, int);
+				write(1, &cbuf, 1);
+			}
+			else if (format[i + 1] == '%')
+			{
+				count += 1;
+				cbuf = '%';
+				write(1, &cbuf, 1);
+			}
+			else if (format[i + 1] == 's')
+			{
+				if ((sbuf = va_arg(lst, char *)) != NULL)
+				{
+					for (j = 0; sbuf[j]; j++)
+					{
+						write(1, &sbuf[j], 1);
+						count += 1;
+					}
+				}
+				else
+					return (-1);
+			}
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count += 1;
+		}
+	}
+	va_end(lst);
+	return (count);
+}
