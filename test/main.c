e9513e99a7c0248466c242538df450b76ad9a2a4
maxastuart <maxastuart@gmail.com>
2018-02-27 23:31:51 +0000

    renaming to make room for new structure version with split functions

--- test/main.c
+++ test/main.c
@@ -8,10 +8,16 @@
  */
 int main(void)
 {
-	printf("%. \n");
-	printf("%d\n", printf("Hello %%%. \n"));
-	_printf("Hello %%%. \n");
-	printf("%d\n", _printf("Hello %%%. \n"));
+	printf("%d\n", printf("Hello %i. \n"));
+	printf("%d\n", _printf("Hello %i. \n"));
+	printf("%d\n", printf("Hello %i. \n", 0));
+	printf("%d\n", _printf("Hello %d. \n", 0));
+	printf("%d\n", printf("Hello %i. \n", 67));
+	printf("%d\n", _printf("Hello %i. \n", 67));
+	printf("%d\n", printf("Hello %i. \n", 127));
+	printf("%d\n", _printf("Hello %d. \n", 127));
+	printf("%d\n", 0 % 10);
+	_printf("%d\n", 0 % 10);
 
 	return (0);
 }
