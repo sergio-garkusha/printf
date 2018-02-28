07df76241a5fe28b9b02fb92dbc8fc1494f69456
Sergii Garkusha <328@holbertonschool.com>
2018-02-27 17:28:23 -0800

    Removed interface functions && added doc && added Test case for numeric key (%d or %i) without value

--- _printf.c
+++ _printf.c
@@ -1,54 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int _print_char(va_list ls)
-{
-	char c = va_arg(ls, int);
-	write(1, &c, 1);
-	return(1);
-}
-
-int _print_string(va_list l){}
-
-int _print_int(va_list ls)
-{
-	int n, j, count = 0;
-	int ibuf = va_arg(ls, int);
-	int tmp = ibuf;
-	char *sbuf;
-
-	for (n = 0; tmp / 10 != 0; n++)
-		tmp = tmp / 10;
-
-	sbuf = malloc(sizeof(char) * (n + 1));
-
-	if (sbuf == NULL)
-		exit(-1);
-
-	for (j = 0; ibuf / 10 != 0; j++)
-	{
-		sbuf[j] = (ibuf % 10) + '0';
-		ibuf = ibuf / 10;
-	}
-	sbuf[j] = (ibuf % 10) + '0';
-	for ( ; j >= 0; j--)
-	{
-		write(1, &sbuf[j], 1);
-		count += 1;
-	}
-
-	free(sbuf);
-
-	return (count);
-}
-
-int _print_mod(va_list l){
-	char c = '%';
-	write(1, &c, 1);
-	return (1);
-}
-
-
+/**
+ * _printf - takes a string and args of each '%'
+ * and prints them
+ * @format: initial string containing % +
+ * char denoting type and number of args
+ * @...: variable list of arguments
+ *
+ * Return: number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
 	int i, j;
@@ -96,6 +57,11 @@ int main(void)
 
 	printf("Numeric:\n");
 	printf("============\n");
+/* %i without param */
+	code = printf("Std: %i.\n");
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %i.\n");
+	printf("Our Returns: %d\n", code);
 
 	code = printf("Std: %i.\n", 0);
 	printf("Std Returns: %d\n", code);
